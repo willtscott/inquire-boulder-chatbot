7125530b8bb1df06481c59323f68e41b1a46581d
Will Scott <wtscott@gmail.com>
2019-03-01 19:54:52 -0500

    Updated Dockerfile with environmental variables

--- src/main.py
+++ src/main.py
@@ -25,4 +25,4 @@ if __name__ == '__main__':
 #   This is used when running locally only. When deploying to Google App
 #   Engine, a webserver process such as Gunicorn will serve the app. This
 #   can be configured by adding an `entrypoint` to .yaml file.
-    app.run(host='0.0.0.0', port=8080, debug = True)
+    app.run(host='0.0.0.0', port=8080, debug = False)
