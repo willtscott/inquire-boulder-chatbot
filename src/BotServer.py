f8382c865f24fbcb39ce177aebd6f54463056ff1
Will Scott <wtscott@gmail.com>
2019-04-02 18:45:48 -0600

    Updated QnA separation and hand-cleaned data

--- src/BotServer.py
+++ src/BotServer.py
@@ -7,7 +7,7 @@ import pandas as pd
 import processing
 
 import json
-import dialogflow
+# import dialogflow
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -50,42 +50,46 @@ class BotServer:
         index, similarity = self.most_similar_sklearn(query)   
         return self.faq.answer.iloc[index]
 
-    def detect_intent_texts(self, project_id, session_id, text, language_code):
-        """Given parameters of the Dialogflow project ID, session ID, user entry text, and language code ('en' for English), returns a fulfillment text based on detected intent from Dialogflow API."""
-        session_client = dialogflow.SessionsClient()
-        session = session_client.session_path(project_id, session_id)
+#     def detect_intent_texts(self, project_id, session_id, text, language_code):
+#         """Given parameters of the Dialogflow project ID, session ID, user entry text, and language code ('en' for English), returns a fulfillment text based on detected intent from Dialogflow API."""
+#         session_client = dialogflow.SessionsClient()
+#         session = session_client.session_path(project_id, session_id)
 
-        if text:
-            text_input = dialogflow.types.TextInput(
-                text=text, language_code=language_code)
-            query_input = dialogflow.types.QueryInput(text=text_input)
-            response = session_client.detect_intent(
-                session=session, query_input=query_input)
-            print('=' * 20)
-            print('Query text: {}'.format(response.query_result.query_text))
-            print('Detected intent: {} (confidence: {})\n'.format(
-                response.query_result.intent.display_name,
-                response.query_result.intent_detection_confidence))
-            print('Fulfillment text: {}\n'.format(
-                response.query_result.fulfillment_text))
+#         if text:
+#             text_input = dialogflow.types.TextInput(
+#                 text=text, language_code=language_code)
+#             query_input = dialogflow.types.QueryInput(text=text_input)
+#             response = session_client.detect_intent(
+#                 session=session, query_input=query_input)
+#             print('=' * 20)
+#             print('Query text: {}'.format(response.query_result.query_text))
+#             print('Detected intent: {} (confidence: {})\n'.format(
+#                 response.query_result.intent.display_name,
+#                 response.query_result.intent_detection_confidence))
+#             print('Fulfillment text: {}\n'.format(
+#                 response.query_result.fulfillment_text))
 
-            return response.query_result.fulfillment_text
+#             return response.query_result.fulfillment_text
     
     def bot_dialog(self, request):
-        """Given the argument POST request, parse it according to json or form data, print the locally-determined Dialogflow API intent, and return a json response based on sklearn matching with the FAQ."""
+        """Given the argument POST request, parse it according to json or form data, print the locally-determined Dialogflow API intent, and return a json response based on sklearn matching within the FAQ."""
         if request.is_json:
             req = request.get_json(force=True)
             message = req.get('queryResult').get('queryText')
         else:
             message = request.form['message']
 
-        project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
-        fulfillment_text = self.detect_intent_texts(project_id, "unique", message, 'en')
+#         project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+#         fulfillment_text = self.detect_intent_texts(project_id, "unique", message, 'en')
 
         response_text = self.match_query(message)
-        print("LOCAL_MATCH: " + response_text)
 
         if request.is_json:
+            print('-' * 20)
+            print(req.get('queryResult').get('intent').get('displayName'))
+#             parsed = json.loads(req)
+#             print(json.dumps(parsed, indent=4, sort_keys=True))
+            
             return jsonify({
                 "fulfillmentText": response_text,
                 "fulfillmentMessages": [
