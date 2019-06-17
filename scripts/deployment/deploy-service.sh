723ba49d19e19f74cb837118f808fc1424c25a20
Will Scott <wtscott@gmail.com>
2019-06-17 09:20:57 -0600

    Added version numbering and beautified ReST website interface

--- scripts/deployment/deploy-service.sh
+++ scripts/deployment/deploy-service.sh
@@ -3,7 +3,7 @@
 # Run on cloud environment
 
 export PROJECT_ID=inquire-boulder-faq
-export IMAGE_NAME=willtscott/inquire-boulder-bot:v3
+export IMAGE_NAME=willtscott/inquire-boulder-bot:$TAG_NAME
 export NUM_NODES=1
 export ZONE=us-central1-b
 export CLUSTER_NAME=bot-cluster
@@ -14,6 +14,6 @@ gcloud config set compute/zone $ZONE
 gcloud container clusters create $CLUSTER_NAME --num-nodes=$NUM_NODES
 kubectl run $DEPLOYMENT_NAME --image=$IMAGE_NAME --port 8080
 kubectl expose deployment $DEPLOYMENT_NAME --type=LoadBalancer --port 80 --target-port 8080
-sleep 1m
+sleep 90s
 kubectl get service
 echo "***Update webhook URL in Dialogflow Fulfilment section with: URL + '/dialog'"
